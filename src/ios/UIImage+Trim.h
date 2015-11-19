8a6317e0f652626072a2bacdd81dd573c552b236
RealBrubru <falecomigo@brubru.com.br>
2015-11-19 02:06:35 -0200

    Create UIImage+Trim.h

---
+++ src/ios/UIImage+Trim.h
@@ -0,0 +1,19 @@
+//
+//  UIImage+Trim.h
+//  Separados no Nascimento
+//
+//  Created by Bruno Oliveira on 11/19/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Trim)
+
+- (UIEdgeInsets)transparencyInsetsRequiringFullOpacity:(BOOL)fullyOpaque;
+- (UIEdgeInsets)transparencyInsetsByCuttingWhitespace:(UInt8)tolerance;
+- (UIImage *)imageByTrimmingTransparentPixels;
+- (UIImage *)imageByTrimmingTransparentPixelsRequiringFullOpacity:(BOOL)fullyOpaque;
+- (UIImage *)imageByTrimmingWhitePixelsWithOpacity:(UInt8)tolerance;
+
+@end
