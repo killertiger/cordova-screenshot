8e3a353658664a83f39fec92745309efe8ef52e4
killertiger <killertiger@gmail.com>
2015-11-18 22:47:28 -0200

    Update Screenshot.java

--- src/android/Screenshot.java
+++ src/android/Screenshot.java
@@ -118,6 +118,32 @@ public class Screenshot extends CordovaPlugin {
 								folder.mkdirs();
 							}
 
+							//ALTERAÇÕES *************************
+							dasateasteasseasjo
+							jiojij
+							Image source = bitmap;
+
+							boolean flag = false ;
+							int upperBorder = -1 ; 
+							do{
+								upperBorder ++ ;
+								for (int c1 =0 ; c1 < source.getWidth() ; c1++){
+									if(source.getRGB(c1, upperBorder) != Color.white.getRGB() ){
+										flag = true;
+										break ;
+									}
+								}
+
+								if (upperBorder >= source.getHeight())
+									flag = true ;
+							}while(!flag) ;
+
+							BufferedImage destination = new BufferedImage(source.getWidth(), source.getHeight() - upperBorder, BufferedImage.TYPE_INT_ARGB) ;
+							destination.getGraphics().drawImage(source, 0, upperBorder*-1, null) ;
+							bitmap = destination;
+
+							//ALTERAÇÕES *************************
+
 							File f = new File(folder, fileName + "."+format);
 
 							FileOutputStream fos = new FileOutputStream(f);
