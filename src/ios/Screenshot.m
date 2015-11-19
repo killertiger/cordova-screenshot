f17d663985d875a77166d9f20b033aa418dcf236
RealBrubru <falecomigo@brubru.com.br>
2015-11-19 01:19:01 -0200

    Update Screenshot.m

--- src/ios/Screenshot.m
+++ src/ios/Screenshot.m
@@ -93,8 +93,7 @@
 	    for (int y=0; y<height; y++) {
 	        for (int x=0; x<width; x++) {
 	            int pixelIndex = (width * y + x) * 4 /* 4 for A, R, G, B */;
-	            NSLog(@"data pixel: ");
-	            if (data[pixelIndex] != 0) { //Alpha value is not zero; pixel is not transparent.
+	            if (data[pixelIndex] != 255) { //Alpha value is not zero; pixel is not transparent.
 	                if (x < lowX) lowX = x;
 	                if (x > highX) highX = x;
 	                if (y < lowY) lowY = y;
